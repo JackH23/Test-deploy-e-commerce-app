7f5de29bcd720eb0b68fa01a1d5c70e9c39e19d5
JackH23 <sihalardjacky@gmail.com>
2024-10-20 09:42:47 +0700

    same site none in logout and login

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -1,6 +1,14 @@
 async function userLogout(req, res) {
     try {
-        res.clearCookie("token");
+        // res.clearCookie("token");
+
+        const tokenOption = {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        };
+
+        res.clearCookie("token",tokenOption)
 
         res.json({
             message: "Log out successfully",
